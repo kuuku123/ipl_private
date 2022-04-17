fb2612e9a168dbe0716b2af2a581f394e1d78db2
kuuku123 <dohyun0047@gmail.com>
2022-04-17 13:27:08 +0900

    Spring Batch working + lomboksettings

---
+++ ipl-dashboard/src/main/java/io/tony/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,11 @@
+package io.tony.ipldashboard.data;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import io.tony.ipldashboard.model.Match;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput,Match>{
+    
+}
