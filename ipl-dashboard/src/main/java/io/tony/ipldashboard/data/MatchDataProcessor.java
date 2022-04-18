fe93bc9e65c42bafac4ea8f0174ff5675cc498f5
kuuku123 <dohyun0047@gmail.com>
2022-04-18 13:39:34 +0900

    Spring Batch process implemented

--- ipl-dashboard/src/main/java/io/tony/ipldashboard/data/MatchDataProcessor.java
+++ ipl-dashboard/src/main/java/io/tony/ipldashboard/data/MatchDataProcessor.java
@@ -6,6 +6,48 @@ import org.springframework.batch.item.ItemProcessor;
 
 import io.tony.ipldashboard.model.Match;
 
+import java.time.LocalDateTime;
+
 public class MatchDataProcessor implements ItemProcessor<MatchInput,Match>{
+    
+    private static final Logger log = LoggerFactory.getLogger();
+
+    @Override
+    public Match process(MatchInput matchInput) throws Exception {
+        Match match = new Match();
+        match.setId(matchInput.getId());
+        match.setCity(matchInput.getCity());
+
+        match.setDate(LocalDateTime.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+        match.setVenue(matchInput.getVenue());
+
+        //Set Team1 and Team2 depnding on the innings order
+        String firstInningsTeam, secondInningsTeam;
+
+        if ("bat".equals(matchInput.getToss_decision())) {
+            firstInningsTeam = matchInput.getToss_winner();
+            secondInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : matchInput.getTeam1();
+        }
+        else {
+            secondInningsTeam = matchInput.getToss_winner();
+            firstInningsTeam = matchInput.getToss_winner().equals(matchInput.getTeam1())
+                    ? matchInput.getTeam2() : matchInput.getTeam1();
+        }
+
+        match.setTeam1(firstInningsTeam);
+        match.setTeam2(secondInningsTeam);
+
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setResult(matchInput.getResult());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(matchInput.getUmpire2());
+
+        return null;
+    }
+
     
 }
--- ipl-dashboard/src/main/java/io/tony/ipldashboard/model/Match.java
+++ ipl-dashboard/src/main/java/io/tony/ipldashboard/model/Match.java
@@ -3,25 +3,24 @@ package io.tony.ipldashboard.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class Match {
 
     private String id;
     private String city;
-    private String date;
-    private String player_of_match;
+    private LocalDateTime date;
+    private String playerOfMatch;
     private String venue;
-    private String neutral_venue;
     private String team1;
     private String team2;
-    private String toss_winner;
-    private String toss_decision;
-    private String winner;
+    private String tossWinner;
+    private String tossDecision;
+    private String matchWinner;
     private String result;
-    private String result_margin;
-    private String eliminator;
-    private String method;
+    private String resultMargin;
     private String umpire1; 
     private String umpire2;   
 }
